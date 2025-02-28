42e65544032bb6f8320a7a40ee0eee11a806fd5e
Tarcianne S.Rocha <tarctata26@gmail.com>
2025-02-28 16:35:42 -0300

    Add files via upload

---
+++ 0ate10.js
@@ -0,0 +1,5 @@
+function contagem(){
+for (let i = 0; i <= 10; i++) 
+    console.log(i);
+}
+contagem()
---
+++ imparoupar.js
@@ -0,0 +1,9 @@
+let numero = prompt("escolha um númmero")
+function verificarParImpar() {
+    if (numero % 2 === 0) {
+      alert(numero + " é par");
+    } else {
+      alert(numero + " é ímpar");
+    }
+  }
+verificarParImpar()
